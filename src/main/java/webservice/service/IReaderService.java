646197994c60017f96dc22f6f1de1ad5cfea0497
Sun Hao <firefoxbrower@sina.com>
2018-10-21 11:14:35 +0800

    删除额外的文件

--- src/main/java/webservice/service/IReaderService.java
+++
@@ -1,9 +0,0 @@
-package webservice.service;
-import websvr.bean.Reader;
-import java.util.List;
-
-public interface IReaderService {
-    
-    public Reader getReader(String name, String password);
-    public List<Reader> getReaders();
-}
